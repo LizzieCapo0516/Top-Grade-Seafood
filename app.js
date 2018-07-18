f4c11eeb5f283a8229a7e14eadc827d86d6d1b12
Lizzie Capo <elizabeth.mulvihill@gmail.com>
2018-07-18 15:46:21 -0400

    Created cards for crabs

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
+var mainbody = document.getElementById ("mainBody");
 
-var arr = [1,2,3]
+var crabStore = 
+[
+    {"product": "Jumbo Crab", "price":"36.95"},
+    {"product": "Blue Crab", "price":"33.95"},
+    {"product": "Green Crab", "price":"23.95"},
+    {"product": "Yellow Crab", "price":"43.95"},
+    {"product": "Yellow Crab", "price":"43.95"},
+    {"product": "Yellow Crab", "price":"43.95"},
+];
 
-var obj = {}
+var cardOut = "";
 
+var cardDeck = "<div class='card-deck'>"
+var card = "<div class='card'>"
+var cardBody = "<div class='card-body'>"
+
+var closeDiv = "</div>"
+
+crabStore.forEach(function(e){
+    var prod = "<h5 class='card-title'>" + e.product + "</h5>";
+    var price =  "<p class='card-text'>Price: " + e.price + "</p>";
+    cardOut = cardOut + card + cardBody + prod + price + closeDiv + closeDiv;
+    
+});
+
+// var htmlOutput = jumboCrabOutput + blueCrabOut;
+
+mainbody.innerHTML = cardDeck + cardOut + closeDiv;
 
 
