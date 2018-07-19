7c5fae32bb78c4467c529df1f57bec6485aecfa1
Lizzie Capo <elizabeth.mulvihill@gmail.com>
2018-07-19 15:55:47 -0400

    Update app to load seafood dynamically

--- app.js
+++ app.js
@@ -1,40 +1,54 @@
-var mainbody = document.getElementById ("mainBody");
 
-var crabStore = 
-[
-    {"product": "Jumbo Crab", "price":"36.95"},
-    {"product": "Blue Crab", "price":"33.95"},
-    {"product": "Green Crab", "price":"23.95"},
-    {"product": "Yellow Crab", "price":"43.95"},
-    {"product": "Yellow Crab", "price":"43.95"},
-    {"product": "Yellow Crab", "price":"43.95"},
-    {"product": "Yellow Crab", "price":"44.95"},
-    {"product": "Yellow Crab", "price":"44.95"},
-];
 
-var num = 0;
-var stack = "";
-var cardOut = "";
-var closeDiv = "</div>"
-var cardTag = "<div class='card col-3'>"
-var cardDeckTag = "<div class='card-deck mt-4'>"
-var cardBodyTag = "<div class='card-body'>"
 
-crabStore.forEach(function(e){
+function loadPage(cat){
+    var crabs = [];
+    console.log("hello filter");
 
-    var prod = "<h5 class='card-title'>" + e.product + "</h5>";
-    var price =  "<p class='card-text'>Price: " + e.price + "</p>";
-    cardOut = cardOut + cardTag + cardBodyTag + prod + price + closeDiv + closeDiv;
-
-    num++;
-    var rem = num % 3;
-    console.log("remainder: " + rem);
-
-    if (rem == 0 || crabStore.length == num){
-        stack = stack + cardDeckTag + cardOut + closeDiv;
-        cardOut = "";
-    }
+    seaFoodStore.forEach(
+        el => {
+            console.log("category: ", el.category);
+            if (el.category === cat)
+            {
+                crabs.push(el);
+            } 
+            console.log("crab count: ", crabs.length);
+        }   
+    );
     
-});
+    renderPage(crabs);
 
-mainbody.innerHTML = stack;
+};
+
+function renderPage(crabs){
+
+    var num = 0;
+    var stack = "";
+    var cardOut = "";
+    var closeDiv = "</div>"
+    var cardTag = "<div class='card col-3'>"
+    var cardDeckTag = "<div class='card-deck mt-4'>"
+    var cardBodyTag = "<div class='card-body'>"
+    var mainbody = document.getElementById ("mainBody");
+    
+    crabs.forEach(function(el){
+    
+        var prod = "<h5 class='card-title'>" + el.product + "</h5>";
+        var price =  "<p class='card-text'>Price: " + el.price + "</p>";
+        cardOut = cardOut + cardTag + cardBodyTag + prod + price + closeDiv + closeDiv;
+    
+        num++;
+        var rem = num % 3;
+        // console.log("remainder: " + rem);
+    
+        if (rem == 0 || crabs.length == num){
+            stack = stack + cardDeckTag + cardOut + closeDiv;
+            cardOut = "";
+        }
+        
+    });
+    
+    mainbody.innerHTML = stack;
+};
+
+
---
+++ seaFoodStore.js
@@ -0,0 +1,15 @@
+var seaFoodStore = 
+[
+    {
+        "category":"crabs",
+        "product": "Jumbo Crab", 
+        "price":"36.95"
+    },
+    { "category":"crabs","product": "Blue Crab", "price":"33.95"},
+    { "category":"crabs","product": "Green Crab", "price":"23.95"},
+    { "category":"crabs","product": "Yellow Crab", "price":"43.95"},
+    { "category":"crabs","product": "Yellow Crab", "price":"43.95"},
+    { "category":"shrimp","product": "Yellow Shrimp", "price":"43.95"},
+    { "category":"shrimp","product": "Yellow Shrimp", "price":"44.95"},
+    { "category":"shrimp","product": "Yellow Shrimp", "price":"44.95"},
+];
