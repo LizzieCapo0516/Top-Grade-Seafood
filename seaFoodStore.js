0d5e98e596e5589c5eebba437e635c52f1f1fd72
Lizzie Capo <elizabeth.mulvihill@gmail.com>
2018-07-19 16:46:43 -0400

    Added images

--- seaFoodStore.js
+++ seaFoodStore.js
@@ -4,15 +4,47 @@ var seaFoodStore =
         "category":"crabs",
         "product": "Jumbo Crab", 
         "price":"36.95",
-        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsvr7pDLbpblfnYR-UjBQ5RSw3hTykxWZ01ZjxryRnlvbvWjf"
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsvr7pDLbpblfnYR-UjBQ5RSw3hTykxWZ01ZjxryRnlvbvWjf",
     },
     
     { 
-        "category":"crabs","product": "Blue Crab", "price":"33.95"},
-    { "category":"crabs","product": "Green Crab", "price":"23.95"},
-    { "category":"crabs","product": "Yellow Crab", "price":"43.95"},
-    { "category":"crabs","product": "Yellow Crab", "price":"43.95"},
+        "category":"crabs",
+        "product": "Blue Crab",
+        "price":"33.95",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsvr7pDLbpblfnYR-UjBQ5RSw3hTykxWZ01ZjxryRnlvbvWjf",
+    },
+    
+    {
+        "category":"crabs",
+        "product": "Green Crab",
+        "price":"23.95",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsvr7pDLbpblfnYR-UjBQ5RSw3hTykxWZ01ZjxryRnlvbvWjf"
+    },
+
+    {
+        "category":"crabs",
+        "product": "Yellow Crab",
+        "price":"43.95",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsvr7pDLbpblfnYR-UjBQ5RSw3hTykxWZ01ZjxryRnlvbvWjf",
+    },
+
+    {
+        "category":"crabs",
+        "product": "Pink Crab",
+        "price":"43.95",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsvr7pDLbpblfnYR-UjBQ5RSw3hTykxWZ01ZjxryRnlvbvWjf",
+    },
+
+    {
+        "category":"crabs",
+        "product": "Red Crab",
+        "price":"43.95",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsvr7pDLbpblfnYR-UjBQ5RSw3hTykxWZ01ZjxryRnlvbvWjf",
+    },
+    
     { "category":"shrimp","product": "Yellow Shrimp", "price":"43.95"},
+    
     { "category":"shrimp","product": "Yellow Shrimp", "price":"44.95"},
+    
     { "category":"shrimp","product": "Yellow Shrimp", "price":"44.95"},
 ];
